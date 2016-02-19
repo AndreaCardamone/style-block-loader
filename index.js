1439a2b599dd23e2cf388a5127c4da9d88f42d37
Andrea Cardamone <Andrea@andrea-laptop>
2016-02-19 18:00:30 +0100

    add less filter to loader

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
  * Author: Andrea Cardamone
  */
 var cheerio = require("cheerio");
+var loaderUtils = require("loader-utils");
 
-module.exports = function(content) {
-	this.cacheable && this.cacheable();
-	this.value = content;
-	
-	$ = cheerio.load(content);
-	var style = $('style').text();
-	
-	return style;
-}
+module.exports = function (content) {
+    this.cacheable && this.cacheable();
+    this.value = content;
+    
+    var     $ = cheerio.load(content),
+        style = $('style'),
+        query = loaderUtils.parseQuery(this.query);
+        
+    if (query.filter && query.filter === 'less') {
+        return style.attr('rel') === 'stylesheet/less'? style.text() : '';
+    } else {
+        return style.text();
+    }
+};
